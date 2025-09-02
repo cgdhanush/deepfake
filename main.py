c679e36a7fca86195d5def72a8738b3759aae9f1
dhanu <dhanu@mail.com>
2025-09-02 08:24:27 +0530

    fixed

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python3
 """
 Main Deepfake bot script.
-Read the documentation to know what cli arguments you need.
+Read the documentation to know what CLI arguments you need.
 """
 
+import argparse
 import logging
 import sys
-from typing import Any
 
-import argparse
 from deepfake import __version__
+from deepfake.deepfake import DeepFake
 from deepfake.exceptions import DeepfakeException
 from deepfake.loggers import setup_logging_pre
-from deepfake.system import  gc_set_threshold, print_version_info
-
-from deepfake.deepfake import DeepFake
+from deepfake.system import gc_set_threshold, print_version_info
 
 logger = logging.getLogger("deepfake")
 
 
 def main() -> None:
-    """
-    This function will initiate the bot and start the loop.
-    :return: None
-    """
+    """Initiate the DeepFake bot and start the appropriate command."""
+    return_code: int = 1
 
-    return_code: Any = 1
-    
     try:
         setup_logging_pre()
         gc_set_threshold()
@@ -34,49 +28,42 @@ def main() -> None:
         parser = argparse.ArgumentParser(description="DeepFake WebApp Control")
         subparsers = parser.add_subparsers(dest="command", required=True)
 
-        # 'start' command
-        start_parser = subparsers.add_parser("start", help="Start the webpage")
-        
-        # 'train' commmand
-        train_parser = subparsers.add_parser("train", help="Start the webpage")
+        # Define subcommands
+        subparsers.add_parser("start", help="Start the web application")
+        subparsers.add_parser("train", help="Start training the model")
+        subparsers.add_parser("create-userdir", help="Create user-data directory")
 
-        # add create-userdir subcommand
-        create_userdir_cmd = subparsers.add_parser(
-            "create-userdir",
-            help="Create user-data directory.",
-        )
-        
-        
-        # '--version' optional flag (global)
+        # Global optional flags
         parser.add_argument("--version", "-v", action="store_true", help="Show the version")
 
         args = parser.parse_args()
 
         if args.version:
             print_version_info()
-            
-        elif args.command == "start":
-            deepfake = DeepFake()
-            deepfake.startup()
+            return_code = 0
 
-        elif args.command == "train":
+        else:
             deepfake = DeepFake()
-            deepfake.startup()
-        
-        elif args.command == "create-userdir":
-            deepfake = DeepFake()
-            deepfake.start_create_userdir()
-            
-    except SystemExit as e:  # pragma: no cover
-        return_code = e
+
+            match args.command:
+                case "start":
+                    deepfake.startup()
+                case "train":
+                    deepfake.startup()  # Replace with `deepfake.train()` if different logic needed
+                case "create-userdir":
+                    deepfake.start_create_userdir()
+
+    except SystemExit as e:  # Exiting normally (argparse)
+        return_code = e.code
     except KeyboardInterrupt:
-        logger.info("SIGINT received, aborting ...")
+        logger.info("SIGINT received, aborting...")
         return_code = 0
     except DeepfakeException as e:
         logger.error(str(e))
         return_code = 2
     except Exception:
         logger.exception("Fatal exception!")
+
     finally:
         sys.exit(return_code)
 
